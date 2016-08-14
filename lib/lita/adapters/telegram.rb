4d7d20101b38e1de97e9e94594091c2112707f70
Jester <jester.dash@gmail.com>
2016-08-14 18:56:29 +0300

    Add chat_service and shut_down

--- lib/lita/adapters/telegram.rb
+++ lib/lita/adapters/telegram.rb
@@ -1,3 +1,5 @@
+require 'lita/adapters/telegram/chat_service'
+
 module Lita
   module Adapters
     class Telegram < Adapter
@@ -10,10 +12,20 @@ module Lita
         @client = ::Telegram::Bot::Client.new(config.telegram_token, logger: ::Logger.new($stdout))
       end
 
+      # Provides an object for Telegram-specific features.
+      def chat_service
+        ChatService.new(@client)
+      end
+
       def run
         client.listen do |message|
           user = Lita::User.find_by_name(message.from.username)
-          user = Lita::User.create(message.from.username) unless user
+          user = Lita::User.create(
+            message.from.id,
+            name: message.from.username,
+            first_name: message.from.first_name,
+            last_name: message.from.last_name
+          ) unless user
 
           chat = Lita::Room.new(message.chat.id)
 
@@ -25,11 +37,18 @@ module Lita
           robot.receive(msg)
         end
       end
+
       def send_messages(target, messages)
         messages.each do |message|
           client.api.sendChatAction(chat_id: target.room.to_i, action: 'typing')
+          sleep 1
           client.api.sendMessage(chat_id: target.room.to_i, text: message)
         end
+      end
+
+      def shut_down
+        log.info 'Shutting Down...'
+        robot.trigger(:disconnected)
       end
 
       Lita.register_adapter(:telegram, self)
---
+++ lib/lita/adapters/telegram/chat_service.rb
@@ -0,0 +1,22 @@
+module Lita
+  module Adapters
+    class Telegram < Adapter
+      # Telegram-specific features made available to +Lita::Robot+.
+      class ChatService
+        attr_accessor :client
+
+        def initialize(client)
+          @client = client
+        end
+
+        def send_keyboard(target, message, keyboard)
+          keyboard = keyboard.map do |key|
+            key.first + ' ' + key.last
+          end
+          markup = ::Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: keyboard, one_time_keyboard: true)
+          client.api.sendMessage(chat_id: target.room.to_i, text: message, reply_markup: markup, parse_mode: 'Markdown', disable_web_page_preview: true)
+        end
+      end
+    end
+  end
+end
